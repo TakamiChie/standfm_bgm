41a2cb573904c539a186d3309974efb6df066817
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-07-09 22:40:13 +0900

    モードフラグを実装

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ parser = ArgumentParser()
 parser.add_argument("--date", default=TODAY, type=lambda s: datetime.datetime.strptime(s, "%Y/%m/%d"), help="yyyy/mm/dd形式の日付。指定がない場合は今日の日付。")
 parser.add_argument("--file", type=str, help="処理対象のファイル名。これを指定した場合dateは無視される")
 parser.add_argument("--bgm", help="BGMに使用するファイル名。指定が無い場合はconfig.ymlで指定したものが使用される。それもない場合はdefault.mp3")
+parser.add_argument("--mode", help="モードの値。未指定時モード無し")
 parser.add_argument("--theme", help="この値は無視されます" )
 parser.add_argument("--title", help="この値は無視されます")
 parser.add_argument("--gui", action="store_true" , help="この値は無視されます")
@@ -30,20 +31,27 @@ with open("config.yml", "r") as f:
     config["path"][n] = Path(os.path.expandvars(v))
 
 print("> set bgm")
-if args.bgm is None:
-  if "bgms" in config:
-    d = config["bgms"][args.date.weekday()]
-    if type(d) is list:
-      args.bgm = d[math.floor(args.date.day / 7)]
-    elif type(d) is str:
-      args.bgm = d
+if args.mode:
+  args.bgm = config["bgmmodes"][args.mode] \
+    if "bgmmodes" in config and args.mode in config["bgmmodes"] else "default.mp3"
+else:
+  if args.bgm is None:
+    if "bgms" in config:
+      d = config["bgms"][args.date.weekday()]
+      if type(d) is list:
+        args.bgm = d[math.floor(args.date.day / 7)]
+      elif type(d) is str:
+        args.bgm = d
+      else:
+        args.bgm = "default.mp3"
     else:
       args.bgm = "default.mp3"
-  else:
-    args.bgm = "default.mp3"
 print(f"BGM={args.bgm}")
 print("> During data loading")
-voicepath = Path(config["path"]["basefolder"] / (datetime.datetime.strftime(args.date, "%Y-%m-%d.mp3") if args.file is None else args.file))
+fnpattern = "%Y-%m-%d"
+if args.mode: fnpattern += f"_{args.mode}"
+fnpattern += ".mp3"
+voicepath = Path(config["path"]["basefolder"] / (datetime.datetime.strftime(args.date, fnpattern) if args.file is None else args.file))
 destpath = Path(config["path"]["destfile"])
 voice = AudioSegment.from_mp3(voicepath)
 bgm = AudioSegment.from_mp3(config["path"]["bgmfolder"] / args.bgm)
